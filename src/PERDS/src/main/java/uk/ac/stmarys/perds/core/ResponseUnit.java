987f7f6631cbba91b31362b439af23b6d5834566
Eoin Donnelly <eoin.donnelly03@gmail.com>
2025-10-06 17:17:19 +0100

    Response Unit class added, taking the id , location , type of vehicle(ie ambulance , fire engine etc) and a boolean representing whether the vehicle is available or not.

--- src/PERDS/src/main/java/uk/ac/stmarys/perds/core/ResponseUnit.java
+++ src/PERDS/src/main/java/uk/ac/stmarys/perds/core/ResponseUnit.java
@@ -3,4 +3,35 @@ package PERDS.src.main.java.uk.ac.stmarys.perds.core;
 
 class ResponseUnit{
 
+
+    private String id;
+    private Location location;
+    private String type;
+    private boolean available;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
