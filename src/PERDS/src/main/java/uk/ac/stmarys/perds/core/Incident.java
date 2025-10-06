ff007d779048a34aceacd4df128119b99013f5c8
Eoin Donnelly <eoin.donnelly03@gmail.com>
2025-10-06 17:16:10 +0100

    Incident class added , takes the information id, the incident description , the location , severity of the incident , the time of the incident and whether not the incident has been resolved represented as a boolean.

--- src/PERDS/src/main/java/uk/ac/stmarys/perds/core/Incident.java
+++ src/PERDS/src/main/java/uk/ac/stmarys/perds/core/Incident.java
@@ -1,6 +1,24 @@
 package PERDS.src.main.java.uk.ac.stmarys.perds.core;
 
 
-class Incident{
+import java.time.LocalDateTime;
+
+
+class Incident {
+
+    private String id;
+    private String description;
+    private boolean resolved;
+    private Location location;
+    private IncidentSeverity incidentSeverity;
+    private LocalDateTime localDateTime;
+
+
+    public String getId() { return id; }
+    public Location getLocation() { return location;}
+    public IncidentSeverity getSeverity() { return incidentSeverity; }
+    public boolean isResolved() { return resolved; }
+    public void setResolved(boolean resolved) { this.resolved = resolved; }
+
 
 }
